444b722c77a02ca0979172acac0dbc429c3b5efe
Pouyan Khabazi <20944041+pkhabazi@users.noreply.github.com>
2024-04-12 10:35:03 +0200

    initial code and repo structure

---
+++ debug.ps1
@@ -0,0 +1,6 @@
+sudo gem build /home/azureuser/microsoft-sentinel-log-analytics-logstash-output-plugin/microsoft-sentinel-log-analytics-logstash-output-plugin.gemspec
+sudo /usr/share/logstash/bin/logstash-plugin install --no-verify
+sudo /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/logstash-to-sentinel.conf
+
+
+# logger -p local4.warn --rfc3164 --tcp -t CEF: "0|NETWORK|YOURFAVORITEFIREWALL|DnsProxyLog|Datacenter|proxy|10.73.4.7|48454|113.1.15.87|443|123|OUT|eu-v20.events.data.microsoft.com|TCP|ALLOW" -P 514 -d -n 127.0.0.1
